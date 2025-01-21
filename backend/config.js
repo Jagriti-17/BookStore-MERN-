671af144959c7626664bfae0de96bb4479dadd0c
jagriti poddar <jagritipoddar17@gmail.com>
2025-01-21 17:43:55 +0530

    Initial commit for MERN project

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://testing:root@book-store-mern.ajk46.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN'
